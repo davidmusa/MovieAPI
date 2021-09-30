df45b743af7773191e6844c277a7185acc3802ed
David M <davidmusa1@hotmail.com>
2021-09-30 15:12:38 -0400

    Adding Ajax

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+
+    /*
+    * Date: 2021-09-30 
+    * Author: David Musa
+    * Description: In this page you can search a specific movie and find the schedule for this movie!
+    */
+
+?>
+
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="style.css" class="style">
+            <title>MovieAPI</title>
+        </head>
+        <body>
+
+            <div>MOVIE SCHEDULE</div>
+
+            <div>
+
+                <input type="text" name="find_movies" id="find_movies">
+                <input type="submit" onclick="find_movies_btn()">
+
+            </div>
+
+            <div id="output_movies">
+
+            </div>
+
+                <script>
+
+                    function find_movies_btn() {
+
+                        var word_input = document.getElementById("find_movies").value;
+                        var endpoint = " https://api.tvmaze.com/search/shows";
+                        var requested_endpoint = endpoint + "?q=" + word_input;
+                        
+
+                        // Create an XMLHttpRequest object
+                        const xhttp = new XMLHttpRequest();
+                        // Define a callback function
+                        xhttp.onload = function() {
+                            document.getElementById("output_movies").innerHTML =
+                            this.responseText;
+                            }
+                        // Send a request    
+                        xhttp.open("GET", "index.php");
+                        xhttp.send();
+                    }
+
+                </script>
+
+        </body>
+    </html>
